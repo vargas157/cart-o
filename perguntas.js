c4301d494c6e1884600ccbbcf2c0bf17598866fd
vargas157 <167428745+vargas157@users.noreply.github.com>
2025-06-27 19:11:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é a capital do Japão?',
+    'Tóquio'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é o oceano mais profundo?',
+    'Pacífico'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é a língua mais falada do mundo?',
+    'Mandarim'
+)
+
+criaCartao(
+    'Conhecimento Gerais',
+    'Qual é o país com a maior população?',
+    'Índia'
+)
